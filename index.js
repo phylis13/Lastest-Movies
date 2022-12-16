e3524c5cd1609b207a24da490f12b28ef5f1ebfa
phylis13 <phylisnjeri54@gmail.com>
2022-12-16 12:08:55 +0300

    change endpoint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchData() {
-  fetch("http://localhost:3000/films")
+  fetch("https://phylis13.github.io/challenge-3/db.json")
     .then((response) => response.json())
-    .then((data) => appendFirstMovie(data));
+    .then((data) => appendFirstMovie(data.films));
 }
 fetchData();
 
@@ -41,9 +41,9 @@ function appendFirstMovie(data) {
 
 //fetches list of movies in the menu section
 function appendMenu() {
-  fetch("http://localhost:3000/films")
+  fetch("https://phylis13.github.io/challenge-3/db.json")
     .then((response) => response.json())
-    .then((data) => menuTitles(data));
+    .then((data) => menuTitles(data.films));
 }
 appendMenu();
 
